f81524a730eec55626fd607c4d61e64efaa14e8a
Chathuranga Wijesinghe <crwijesinghe@gmail.com>
2022-02-27 00:49:27 +0800

    done 16

--- src/test-sample/Utils/Utils.test.ts
+++ src/test-sample/Utils/Utils.test.ts
@@ -1,28 +1,29 @@
-import { IncomingMessage } from "http";
-import { Utils } from "../../app/Utils/Utils";
+import { Utils } from '../../app/Utils/Utils'
+import { IncomingMessage } from 'http';
 
 describe('Utils test suite', () => {
+
     test('getRequestPath valid request', () => {
         const request = {
             url: 'http://localhost:8080/login'
         } as IncomingMessage;
-        const result = Utils.getRequestBasePath(request);
-        expect(result).toBe('login');
+        const resultPath = Utils.getRequestBasePath(request);
+        expect(resultPath).toBe('login');
     });
 
     test('getRequestPath with no path name', () => {
         const request = {
-            url: 'http://localhost:8080'
+            url: 'http://localhost:8080/'
         } as IncomingMessage;
-        const result = Utils.getRequestBasePath(request);
-        expect(result).toBeFalsy();
+        const resultPath = Utils.getRequestBasePath(request);
+        expect(resultPath).toBeFalsy();
     });
 
     test('getRequestPath with no path name', () => {
         const request = {
             url: ''
         } as IncomingMessage;
-        const result = Utils.getRequestBasePath(request);
-        expect(result).toBeFalsy();
+        const resultPath = Utils.getRequestBasePath(request);
+        expect(resultPath).toBeFalsy();
     });
 });
